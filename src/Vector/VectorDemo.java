7b6af593e2c999acfff915a4c132425ee03c2619
Ruturaj Narhe <165364911+ruturajnarhe@users.noreply.github.com>
2024-12-21 13:21:54 +0530

    Update VectorDemo.java
    new commit

--- src/Vector/VectorDemo.java
+++ src/Vector/VectorDemo.java
@@ -5,6 +5,26 @@ public class VectorDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Vector v = new Vector();
+		
+		
+		// Properties-
+		// Vector is an index based data structure
+		// Vector can store different data-types or heterogeneous data-types
+		// Vector can store duplicate elements
+		// Vector can store multiple null values
+		// Vector follows insertion order
+		// Vector does not follows sorting order
+		// Vector are synchronized collection
+		
+		v.add(101);
+		v.add("Ravindra");
+		v.add("Chetan");
+		v.add("Saurabh");
+		v.add(345.6);
+		v.add('r');
+		
+		System.out.println(v);
 	}
 
 }
